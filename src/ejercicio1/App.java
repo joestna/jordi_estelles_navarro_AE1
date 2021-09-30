68e296fba52e879059f15f310a909aba7cf3599f
Jordi <jonana01@floridauniversitaria.es>
2021-09-30 19:56:12 +0200

    Ejercicio 1 completado

---
+++ src/ejercicio1/App.java
@@ -0,0 +1,12 @@
+package ejercicio1;
+
+public class App {
+
+	public static void main(String[] args) {
+		SayHello();
+	}
+	
+	static void SayHello() {
+		System.out.println( "Hola Mundo" );
+	}
+}
