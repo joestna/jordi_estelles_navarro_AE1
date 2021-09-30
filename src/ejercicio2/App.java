e612fe27dc3aee87f87ab8467c537af3b8503c80
Jordi <jonana01@floridauniversitaria.es>
2021-10-01 01:01:54 +0200

    Estilizacion (detalles) del codigo

--- src/ejercicio2/App.java
+++ src/ejercicio2/App.java
@@ -3,9 +3,12 @@ package ejercicio2;
 import java.util.Scanner;
 
 public class App {
+	
+	
 
 	//Ejercicio con algunas ampliaciones utiles
 	public static void main(String[] args) {
+		
 		Scanner input = new Scanner(System.in);
 		
 		String[] grupoAlumnos = new String[6];
@@ -16,15 +19,19 @@ public class App {
 		for( int i = 0; i < cantidadAlumnosEnClase; i++ ) {
 			numeroAlumno = AddStudent( grupoAlumnos, numeroAlumno, input );
 		}
+			
+		ListStudents( grupoAlumnos, numeroAlumno );	
 		
 		input.close();
 		
-		ListStudents( grupoAlumnos, numeroAlumno );		
 	}
+	
+	
 	
 	//Valida que el input introducido es correcto (numero del 1 al 9)
 	//Incluye el input (nombre del estudiante) al grupoAlumnos
 	static int AddStudent( String[] grupoAlumnos, int numeroAlumno,  Scanner input ) {
+		
 		String nombreAlumno = "";
 		boolean error = false;	
 		
@@ -41,30 +48,38 @@ public class App {
 				numeroAlumno ++;
 				grupoAlumnos[ numeroAlumno -1 ] = nombreAlumno;
 				System.out.println( "\nOutput : " + nombreAlumno + " - Incluido correctamente.\n" );
+				
 			}else {
 				System.out.println( "\nOutput : Nombre Incorrecto\n" );
 				error = true;
 			}
+			
 		}while( error );
 		
 		return numeroAlumno;
+		
 	}
+	
 	
 	
 	//Muestra una lista con los estudiantes incluidos en grupoAlumnos
 	static void ListStudents( String[] grupoAlumnos, int numeroAlumno ) {
+		
 		System.out.println( "---\nListado de Alumnos :\n---" );
 				
 		for( int i = 0; i < numeroAlumno ; i++ ) {
 			System.out.println( (i +1) + ". " + grupoAlumnos[i] );
 		}
+		
 		System.out.println( "---" );
 	}
 		
 	
+	
 	//Valida que el input introducido es correcto (numero del 1 al 9)
 	//Define la cantidad de estudiantes que hay en clase
 	static int AddAmountStudents( Scanner input ) {		
+		
 		String regexp = "\\d[1-9]*$";
 		
 		boolean error = false; 
@@ -81,13 +96,16 @@ public class App {
 			if( stringCantidadAlumnosEnClase.matches(regexp) ) {
 				cantidadAlumnosEnClase = Integer.parseInt( stringCantidadAlumnosEnClase );
 				System.out.println( "\nOutput : Numero introducido correctamente : " + cantidadAlumnosEnClase + "\n" );
+			
 			}else if( stringCantidadAlumnosEnClase.equals( "default" ) ){
 				cantidadAlumnosEnClase = 6;
 				System.out.println( "\nOutput : Numero introducido correctamente : " + cantidadAlumnosEnClase + "\n" );
+			
 			}else {
 				System.out.println( "\nOutput : El valor introducido no es un numero ni \" default \"\n" );
 				error = true;
 			}
+			
 		}while( error );
 		
 		return cantidadAlumnosEnClase;		
